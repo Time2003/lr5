5513d79dec6581d128d6d64c7f08e2a34463c843
Time.2003 <manyakhin.28@gmail.com>
2022-03-24 08:03:45 +0300

    otchet

---
+++ 3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Вычислить сумму всех n-значных чисел (1 <= n <= 4).
+import math
+n = [1, 2, 3, 4]
+i = 0
+counter = 0
+for i in range(i, 4):
+    counter += n[i]
+i += 1
+print("сумма всех чисел равна:", counter)
